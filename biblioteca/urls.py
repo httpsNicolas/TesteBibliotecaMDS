45aa39320c94ded49a0c1e51250153adb8e9ebfd
Nícolas <nickollas055@gmail.com>
2025-07-13 18:26:57 -0300

    apenas testando o sonarcloud

--- biblioteca/urls.py
+++ biblioteca/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
+from django.http import HttpResponse
 from django.urls import path, include
 
 urlpatterns = [
+    path('', lambda request: HttpResponse("Página Inicial!")),
     path('admin/', admin.site.urls),
     path('livro/', include('livro.urls')),
     path('auth/', include('usuarios.urls'))
