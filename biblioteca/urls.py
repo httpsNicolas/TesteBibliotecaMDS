9ecd6beba1ba97e70e230cddf1d30885215ea7ce
unknown <wallisonbreno123@gmail.com>
2025-07-17 21:28:13 -0300

    Rota padrão como login

--- biblioteca/urls.py
+++ biblioteca/urls.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 from django.http import HttpResponse
 from django.urls import path, include
+from django.shortcuts import redirect
 
 urlpatterns = [
-    path('', lambda request: HttpResponse("Página Inicial!")),
+    path('', lambda request: redirect('login')),
     path('admin/', admin.site.urls),
     path('livro/', include('livro.urls')),
     path('auth/', include('usuarios.urls'))
