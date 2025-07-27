8440086ae3f949c815bd6fd8fe347b46b62a5c71
unknown <wallisonbreno123@gmail.com>
2025-07-27 19:00:11 -0300

    Definindo os campos do formulário explicitamente

--- livro/forms.py
+++ livro/forms.py
@@ -9,11 +9,21 @@ from datetime import date
 class CadastroLivro(forms.ModelForm):
     class Meta:
         model = Livros
-        fields = "__all__"
+        fields = [
+            'nome',
+            'autor',
+            'co_autor',
+            'data_cadastro',
+            'emprestado',
+            'categoria',
+            'usuario'
+        ]
     
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.fields['usuario'].widget = forms.HiddenInput()
+        self.fields['data_cadastro'].widget = forms.HiddenInput()
+        self.fields['emprestado'].widget = forms.HiddenInput()
 
 class CategoriaLivro(forms.Form):
     nome = forms.CharField(max_length=30)
