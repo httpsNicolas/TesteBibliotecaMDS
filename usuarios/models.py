7cb863ee7362624f6637418191ab4eb98430bce0
unknown <wallisonbreno123@gmail.com>
2025-07-27 19:15:23 -0300

    Removendo comentário com dados sensiveis

--- usuarios/models.py
+++ usuarios/models.py
@@ -1,9 +1,5 @@
 from django.db import models
 
-#user: caio12
-#email: caio@gmail.com
-#senha: teste1234
-
 class Usuario(models.Model):
     nome = models.CharField(max_length=30)
     email = models.EmailField()
